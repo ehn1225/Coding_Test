b59527c6fd34490067b52ff1a7255e712ce43f49
YeChan Lee <ehn1225@naver.com>
2023-03-15 23:05:09 +0900

    [Programmers][이예찬] 2023-03-15 Solves

---
+++ 42883.cpp
@@ -0,0 +1,19 @@
+﻿//Programmers 42883 : 그리디 - 큰 수 만들기
+#include <bits/stdc++.h>
+
+using namespace std;
+string solution(string number, int k) {
+
+    int str_len = number.length();
+    for(int i = 0; i < str_len-1; i++){
+        if (number[i] < number[i+1]) {
+            number.erase(i, 1);
+            i = -1;
+            k--;
+        }
+        if(k == 0) break;
+    }
+    if(k > 0) number.erase(str_len - k, k);
+
+    return number;
+}
--- 42885.cpp
+++ 42885.cpp
@@ -1,41 +1,27 @@
 //Programmers 42885 : 그리디 - 구명보트
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(vector<int> people, int limit) {
+    //일단 몸무게를 기준으로 내림차순 정렬
+    //배열의 시작을 순환하면서 배열에 삽입
     int answer = 0;
-    sort(people.begin(), people.end());
-    queue <int> q;
-    for (auto it = people.begin(); it != people.end(); it++)
-        q.push(*it);
-    int load = 0;
-    int people_count = 0;
-    while (!q.empty()) {
-        if (load + q.front() <= limit && people_count < 2) {
-            load += q.front();
-            q.pop();
-            people_count++;
+    
+    sort(people.begin(), people.end(), greater<>());
+    int bpos = 0;
+    int person_count = people.size();
+    int epos = person_count - 1;
+
+    while(person_count > 0){
+        //제일 무거운 사람을 선택하고, 한 배에 같이 탈 사람을 찾음. 없으면 혼자 탐.
+        int weight = people[bpos++];
+        person_count--;
+        if(weight + people[epos] <= limit){
+            epos--;
+            person_count--;
         }
-        else {
-            load = 0;
-            people_count = 0;
-            answer++;        
-        }    
-    }
-    if (load > 0) {
         answer++;
     }
-
     return answer;
-}
-
-int main() {
-    cout << solution({20,20,20,20,20,20}, 100);
-
-
 }
